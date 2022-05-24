b2f65ed6d72798348786338f15d84f74aadc4aae
phamnhatminh1292001 <66765903+phamnhatminh1292001@users.noreply.github.com>
2022-05-24 16:07:17 +0700

    Update signing_server_sol.py

--- math2/cryptography2/RSA/signing_server_sol.py
+++ math2/cryptography2/RSA/signing_server_sol.py
@@ -41,25 +41,6 @@ def take_dict(s):
     str=json.loads(str)
     return str
 
-def is_pkcs7_padded(message):
-    padding = message[-message[-1]:]
-    return all(padding[i] == len(padding) for i in range(0, len(padding)))
-
-def decrypt_flag(shared_secret: int, iv: str, ciphertext: str):
-    # Derive AES key from shared secret
-    sha1 = hashlib.sha1()
-    sha1.update(str(shared_secret).encode('ascii'))
-    key = sha1.digest()[:16]
-    # Decrypt flag
-    ciphertext = bytes.fromhex(ciphertext)
-    iv = bytes.fromhex(iv)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    plaintext = cipher.decrypt(ciphertext)
-
-    if is_pkcs7_padded(plaintext):
-        return unpad(plaintext, 16).decode('ascii')
-    else:
-        return plaintext.decode('ascii')
 
 #if your option is "sign", and your input is s, you will receive s^d (mod N)
 #you have m^e (mod N), then just let s=m^e and you get m. Done
