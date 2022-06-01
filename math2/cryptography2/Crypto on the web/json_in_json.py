1ebe8174039d9237e7461b3628149852d79a0f1f
phamnhatminh1292001 <66765903+phamnhatminh1292001@users.noreply.github.com>
2022-06-01 17:40:15 +0700

    Update json_in_json.py

--- math2/cryptography2/Crypto on the web/json_in_json.py
+++ math2/cryptography2/Crypto on the web/json_in_json.py
@@ -11,6 +11,10 @@ def authorise(token):
     url = "http://web.cryptohack.org/json-in-json/authorise/"
     r = requests.get(url+token+'/')
     return r.json()
+#b={"a":True,"a":False}
+#print(b)
+#result: False
+#with this, in our input, we just need to insert '"minh","Admin":True' into the dictionary string.
 m=create_session("minh"\
 +'", "admin": "'+"True")
 test=authorise(m)
